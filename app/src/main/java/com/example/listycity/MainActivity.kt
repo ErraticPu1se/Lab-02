e7afa375138cb3dbae8f4aa20783ed65c79b4c7c
Cristi <crispeng215@gmail.com>
2026-09-10 23:59:48 -0600

    uh

--- app/src/main/java/com/example/listycity/MainActivity.kt
+++ app/src/main/java/com/example/listycity/MainActivity.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.listycity.ui.theme.ListyCityTheme
-import androidx.compose.ui.tooling.preview.Preview
 
 
 
